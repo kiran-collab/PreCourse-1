aa33ea199621ba96f51d10edbeca259363013ca9
Kiran Davuluri <75129341+kiran-collab@users.noreply.github.com>
2023-07-20 17:27:37 -0400

    Updated Exercise_2.py

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,3 +1,6 @@
+#stack using linked list
+#Time complexity = O(n)
+#Space complexity = O(n) 
 
 class Node:
     def __init__(self, data):
@@ -6,11 +9,34 @@ class Node:
  
 class Stack:
     def __init__(self):
+        self.head=None
+        n=self.head
         
     def push(self, data):
-        
+        newNode=Node(data)
+        if self.head is None:
+            self.head=newNode
+        else:
+            n=self.head
+            while n.next is not None:
+                n=n.next
+            n.next=newNode
+
     def pop(self):
-        
+        popped=None
+        if self.head is None:
+            print("Stack is empty")
+            return None
+        else:
+            n = self.head
+            while n.next is not None:
+                if n.next.next is None:
+                    popped_val = n.next.data
+                    n.next = None
+                else: 
+                    n = n.next
+            return popped_val
+
 a_stack = Stack()
 while True:
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
