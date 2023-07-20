f897e15ee824c640f98c4b9abe76cdf9860360c1
Kiran Davuluri <75129341+kiran-collab@users.noreply.github.com>
2023-07-20 17:26:18 -0400

    Updated Exercise_1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,20 +1,44 @@
+#Stack using array
+
+#Time Complexity = O(1)
+#Space Complexity = O(1)
+
 class myStack:
-  #Please read sample.java file before starting.
-  #Kindly include Time and Space complexity at top of each file
      def __init__(self):
-         
+     	self.Stack=[]
+
      def isEmpty(self):
-         
+     	if len(self.Stack)<=0:
+     		return True
+     	else:
+     		return False
+
      def push(self, item):
+     	if(!self.isEmpty()):
+     		print("Stack is full")
+     		return 
+     	self.Stack.append(item)
          
      def pop(self):
-        
+        if(self.isEmpty()):
+        	print("Stack is empty")
+        else:
+        	item=self.Stack[-1]
+        	del self.Stack[-1]      #del top element of the stack
+        	return item
         
      def peek(self):
+     	if(self.isEmpty()):
+     		print("Stack is empty")
+     		return
+     	else:
+     		return self.Stack[-1]  #top element of the stack
         
      def size(self):
+     	return len(self.Stack)
          
      def show(self):
+     	return self.Stack
          
 
 s = myStack()
